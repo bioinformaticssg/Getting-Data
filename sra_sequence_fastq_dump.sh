b6e93fac40d261644f2fe5f3638eb3624f90943d
Stacey Borrego <staceyborrego@gmail.com>
2018-01-29 12:42:24 -0800

    Ready for qsub

--- sra_sequence_fastq_dump.sh
+++ sra_sequence_fastq_dump.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+#$ -N sra_sequence_dl      # name of the job
+#$ -o /data/users/$USER/BioinformaticsSG/Getting-Data    # contains what would normally be printed to stdout (the terminal). The file sra11.out go the location indicated by the path.
+#$ -e /data/users/$USER/BioinformaticsSG/Getting-Data   # file name to print standard error messages to. These messages would normally be printed to the the terminal. The file sra11.err wil go the location indicated by the path.
+#$ -q free64,som,asom       # request cores from the free64, som, asom queues.
+#$ -pe openmp 8-64          # request parallel environment. You can include a minimum and maximum core count.
+#$ -m beas                  # send you email of job status (b)egin, (e)rror, (a)bort, (s)uspend
+#$ -ckpt blcr               # (c)heckpoint: writes a snapshot of a process to disk, (r)estarts the process after the checkpoint is complete
+
+module load blcr
+module load SRAToolKit
+
+# Here we are assigning variables with paths
+DIR=/data/users/$USER/BioinformaticsSG/Getting-Data/
+DATA=${DIR}/data_1
+
+mkdir ${DATA}
+# Here we are changing our current directory to the new file
+cd ${DATA}
+
 nstart="3744659"
 nstop="3744667"
 
